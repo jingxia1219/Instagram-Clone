6226a1a86d90000e45c14a4241dcb5b9489698a2
Jing <jingxia1219@gmail.com>
2018-08-07 13:21:18 -0700

    create user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+namespace :api, defaults: {format: :json} do
+  resources :users do
+    resources :posts, only[:index]
+  end
+  resource :session, only:[:create, :destroy, :show]
+  resources :posts, only:[:create, :destroy, :index, :show] do
+    resources :comments, only:[:create, :update, :destroy]
+  end 
 end
---
+++ frontend/insta_pic.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// import Root from './components/root';
+// import configureStore from './store/store';
+//
+// document.addEventListener("DOMContentLoaded", ()=>{
+//   const store = configureStore();
+//   ReactDOM.render(<Root store={store}/>, document.getElementById("root"));
+// });
