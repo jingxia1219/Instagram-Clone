72d998743f2b3042bec7724d9dd3616fbf75d094
Jing <jingxia1219@gmail.com>
2018-08-07 15:25:28 -0700

    Finish User and Session controller and User Model backend Auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      render '/api/users/show'
+    else
+      render json:@user.errors.messages, status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
+
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,26 @@
+class Api::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.messages, status: 422
+    end
+  end
+
+    private
+    def user_params
+      params.require(:user).permit(:username,:password,:email,:bio)
+    end
+
+end
