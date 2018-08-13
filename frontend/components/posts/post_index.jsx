f357273af715f3f4e1bf9c60657da1f9f36225bf
Jing <jingxia1219@gmail.com>
2018-08-13 16:21:21 -0700

    Add like, comment and bookmark logo without functionality

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -32,11 +32,15 @@ class PostIndex extends React.Component {
     console.log("THIS.props:", this.props.users);
     // debugger;
       const posts = this.props.posts.map( post=> {
-        console.log("THIS.props,users[post.author_id]:", this.props.users[post.author_id-1]);
+        // console.log("THIS.props,users[post.author_id]:", this.props.users[post.author_id-1]);
         return (
         <div key={`div-${post.id}`} className='post-item'>
           <li key={post.id}>
+            <h6 key={`post-user-${post.id}-1`} className='post-user'>{this.props.users[post.author_id-1].username}</h6>
             <img  key={`img-${post.id}`} className="post-picture" src={post.photoUrl}/>
+            <img key={`heart-${post.id}`} className='like-heart' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39100399_1778594592235747_85684206713176064_n.jpg?_nc_cat=0&oh=7050129bffbc2b6d7a99ba00f5af8a52&oe=5C0BE074"/>
+            <img key={`cooment-${post.id}`} className='comment-logo' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39024220_1778594585569081_1067191603736608768_n.jpg?_nc_cat=0&oh=cdc88d2fca2d74c4e2d47fede3d606a5&oe=5BFFB142"/>
+            <img key={`bookmark-${post.id}`} className='bookmark-logo' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39165058_1778626478899225_3517323601172758528_n.jpg?_nc_cat=0&oh=bfdb93bf357be2050ae934221ddaf26f&oe=5BC52A9C"/>
             <h6 key={`post-user-${post.id}`} className='post-user'>{this.props.users[post.author_id-1].username}:<span className='post-description'>{post.description}</span></h6>
 
           </li>
