747ff89ce7ff5aa8808da110a1cdc6fdf416e052
Jing <jingxia1219@gmail.com>
2018-09-17 11:10:37 -0700

    changed position

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -8,7 +8,7 @@ const Greeting = ({ currentUser, logout, demoLogin }) => {
       <img className='iphone-screen' src='https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png'/>
       <img className='pic-one' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38774870_1770092733085933_601809438831017984_n.jpg?_nc_cat=0&oh=5a55211a4ea9706e80976e81e66dc2c6&oe=5BC8CD24"/>
       <Login />
-    </div>
+      </div>
   );
   const personalGreeting = () => (
     <hgroup className="header">
