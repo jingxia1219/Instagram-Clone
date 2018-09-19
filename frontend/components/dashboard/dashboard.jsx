ce82ae010cc90486b183f00d8f5fde2e7c35115e
Jing <jingxia1219@gmail.com>
2018-09-19 13:56:25 -0700

    swithced map with power off button

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -8,31 +8,29 @@ import Follow from '../users/follow_container';
 
 const personalGreeting = ({currentUser, logout, posts, modalOpen}) => {
 
-  return (
-    <div className='post-body'>
+  return <div className="post-body">
+      <hgroup className="header-group">
+        <div className="dashboard-navbar">
+          <img className="insta-logo" src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png" />
+          <img className="instapic-small" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38749082_1769735009788372_243286579940950016_n.jpg?_nc_cat=0&oh=6523cec7952760d9608eb243a59981c1&oe=5C121671" />
+          <img className="search-img" src="https://www.freeiconspng.com/uploads/search-icon-png-1.png" />
+          <input className="search-box" type="text" placeholder="Search" />
+          <button className="create-post-modal-dashboard" onClick={modalOpen}>
+            New Post
+          </button>
+          <Modal />
+          <img className="empty-heart" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38874877_1771545116274028_9140465076254277632_n.jpg?_nc_cat=0&oh=c04991f09b8c3e22fdc10f87a77feda7&oe=5BFA37A8" />
+          <Link to={`/user/${currentUser.id}`}>
+            <img className="user-icon" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38792536_1771544876274052_120399194711130112_n.jpg?_nc_cat=0&oh=cf5c93cafd2f4f659a349a05660ddbff&oe=5C0F99EE" />
+          </Link>
+        <img onClick={logout} className="compass" title="Log out" src="https://png2.kisspng.com/20180401/izw/kisspng-computer-icons-arrow-exit-5ac1aa4a3a2154.0432564515226414822381.png" />
+        </div>
+      </hgroup>
 
-    <hgroup className="header-group">
-    <div className='dashboard-navbar'>
-    <img className='insta-logo' src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png" ></img>
-    <img className="instapic-small"  src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38749082_1769735009788372_243286579940950016_n.jpg?_nc_cat=0&oh=6523cec7952760d9608eb243a59981c1&oe=5C121671'></img>
-    <img className='search-img' src='https://www.freeiconspng.com/uploads/search-icon-png-1.png'/>
-    <input className="search-box" type="text" placeholder="Search"></input>
-      <button className='create-post-modal-dashboard' onClick={modalOpen}>New Post</button>
-      <Modal />
-      <img className="empty-heart" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38874877_1771545116274028_9140465076254277632_n.jpg?_nc_cat=0&oh=c04991f09b8c3e22fdc10f87a77feda7&oe=5BFA37A8" />
-        <Link to={`/user/${currentUser.id}`}>
-              <img className='user-icon'
-                src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38792536_1771544876274052_120399194711130112_n.jpg?_nc_cat=0&oh=cf5c93cafd2f4f659a349a05660ddbff&oe=5C0F99EE"/>
-            </Link>
-      <img onClick={logout} className='compass' title="Log out" src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38780715_1771545332940673_3483777273776046080_n.jpg?_nc_cat=0&oh=3814f0874c97771db9801d8e4145847a&oe=5BFC818F'/>
-    </div>
-  </hgroup>
+      <PostIndexContainer posts={posts} />
 
-<PostIndexContainer posts={posts}/>
-
-<Follow />
-  </div>
-);
+      <Follow />
+    </div>;
 };
 
 export default personalGreeting;
--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -19,47 +19,45 @@ componentDidMount() {
       const postsByUser = this.props.postsByUser.map( post =>
         <div key={`picture-box-key-${post.id}`} className={`picture-box-${post.id}`}><img className='user-show-post' key={`user-show-post-${post.id}`} src={post.photoUrl} /></div>
        );
-      return (
-        <div>
-        <hgroup className="header-group">
-        <div className='dashboard-navbar'>
-        <img className='insta-logo' src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png" ></img>
-        <Link to='/dashboard'>
-          <img className="user-show-instapic-small"  src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38749082_1769735009788372_243286579940950016_n.jpg?_nc_cat=0&oh=6523cec7952760d9608eb243a59981c1&oe=5C121671'></img>
-          </Link>
-        <img className='search-img' src='https://www.freeiconspng.com/uploads/search-icon-png-1.png'/>
-        <input className="search-box" type="text" placeholder="Search"></input>
-          <button className='create-post-modal-dashboard' onClick={this.props.openModalPost}>New Post</button>
+      return <div>
+          <hgroup className="header-group">
+            <div className="dashboard-navbar">
+              <img className="insta-logo" src="https://seeklogo.com/images/I/instagram-logo-A807AD378B-seeklogo.com.png" />
+              <Link to="/dashboard">
+                <img className="user-show-instapic-small" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38749082_1769735009788372_243286579940950016_n.jpg?_nc_cat=0&oh=6523cec7952760d9608eb243a59981c1&oe=5C121671" />
+              </Link>
+              <img className="search-img" src="https://www.freeiconspng.com/uploads/search-icon-png-1.png" />
+              <input className="search-box" type="text" placeholder="Search" />
+              <button className="create-post-modal-dashboard" onClick={this.props.openModalPost}>
+                New Post
+              </button>
 
-          <img className="empty-heart" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38874877_1771545116274028_9140465076254277632_n.jpg?_nc_cat=0&oh=c04991f09b8c3e22fdc10f87a77feda7&oe=5BFA37A8" />
+              <img className="empty-heart" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38874877_1771545116274028_9140465076254277632_n.jpg?_nc_cat=0&oh=c04991f09b8c3e22fdc10f87a77feda7&oe=5BFA37A8" />
 
-                  <img className='in-user-icon'
-                    src="https://cdn0.iconfinder.com/data/icons/users-android-l-lollipop-icon-pack/24/user-128.png"/>
+              <img className="in-user-icon" src="https://cdn0.iconfinder.com/data/icons/users-android-l-lollipop-icon-pack/24/user-128.png" />
 
-          <img onClick={this.props.logout} className='compass' src='https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/38780715_1771545332940673_3483777273776046080_n.jpg?_nc_cat=0&oh=3814f0874c97771db9801d8e4145847a&oe=5BFC818F'/>
-        </div>
-
-      </hgroup>
-        <div className='profile-body'>
-          <div className='profile-picture-container'>
-            <img className='default-user-icon'
-              onClick={this.props.openModalProfile}
-              src={this.props.users[this.props.userId].avatarUrl} />
+              <img onClick={this.props.logout} className="compass" src="https://png2.kisspng.com/20180401/izw/kisspng-computer-icons-arrow-exit-5ac1aa4a3a2154.0432564515226414822381.png" />
+            </div>
+          </hgroup>
+          <div className="profile-body">
+            <div className="profile-picture-container">
+              <img className="default-user-icon" onClick={this.props.openModalProfile} src={this.props.users[this.props.userId].avatarUrl} />
+            </div>
+            <div className="bio">
+              <div>
+                <h2 className="user-show-username">
+                  {this.props.users[this.props.userId].username}
+                </h2>
+                <h2 className="bio">Bio:</h2>
+              </div>
+              <h3 className="bio-text">
+                {this.props.users[this.props.userId].bio}
+              </h3>
+              <Modal />
+            </div>
           </div>
-          <div className='bio'>
-            <div>
-          <h2 className='user-show-username'>{this.props.users[this.props.userId].username}</h2>
-          <h2 className='bio'>Bio:</h2>
-          </div>
-          <h3 className='bio-text'>{this.props.users[this.props.userId].bio}</h3>
-            <Modal/>
-          </div>
-        </div>
-        <div className='user-show-posts'>
-        {postsByUser}
-        </div>
-      </div>
-    );
+          <div className="user-show-posts">{postsByUser}</div>
+        </div>;
     }
 }
 
