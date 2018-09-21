1af2f7280e324d06b9963bcfc1e839b04502e35e
Jing <jingxia1219@gmail.com>
2018-09-20 22:44:08 -0700

    adsfsda

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -26,7 +26,8 @@ class PostIndex extends React.Component {
               </Link></div>
             <h6 key={`post-user-${post.id}-1`} className='post-user-top'>{this.props.users[post.author_id].username}</h6>
           </div>
-            <img  key={`img-${post.id}`} className="post-picture" src={post.photoUrl}/>
+            <div className='post-pic-container'><img  key={`img-${post.id}`} className="post-picture" src={post.photoUrl}/>
+            </div>
             <img key={`heart-${post.id}`} className='like-heart' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39100399_1778594592235747_85684206713176064_n.jpg?_nc_cat=0&oh=7050129bffbc2b6d7a99ba00f5af8a52&oe=5C0BE074"/>
             <img key={`comment-${post.id}`} className='comment-logo' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39024220_1778594585569081_1067191603736608768_n.jpg?_nc_cat=0&oh=cdc88d2fca2d74c4e2d47fede3d606a5&oe=5BFFB142"/>
             <img key={`bookmark-${post.id}`} className='bookmark-logo' src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/39165058_1778626478899225_3517323601172758528_n.jpg?_nc_cat=0&oh=bfdb93bf357be2050ae934221ddaf26f&oe=5BC52A9C"/>
